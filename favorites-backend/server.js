3957751df292f71e45990e3ef1a0e4d454aaf047
lasiru Minruk <lasiru2225@gmail.com>
2025-05-18 10:15:47 +0530

    update backend

--- favorites-backend/server.js
+++ favorites-backend/server.js
@@ -1,11 +1,57 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const app = express();
+
+const allowedOrigins = [
+
+  'http://localhost:5173',  
+  
+];
+
+
 app.use(cors());
 app.use(express.json());
+
+
+// CORS middleware configuration
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('The CORS policy for this site does not allow access from the specified origin.'));
+      }
+    }
+  })
+);
+
+
+// Set up Helmet for Content Security Policy
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "script-src": ["'self'", "https://vercel.live"], // Allow scripts from vercel.live
+      "default-src": ["'self'"],
+    },
+  })
+);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.send('API WORKING');
+});
+
+
+
+
 
 const favoriteRoutes = require("./routes/favoriteRoutes");
 app.use("/api/favorites", favoriteRoutes);
